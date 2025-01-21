7fe8f6f56b8a27754a77ca7ed7b507f4a211a4d7
Heinz <aurelianob808@gmail.com>
2025-01-21 15:19:59 +0800

    07

--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -35,7 +35,9 @@ mod tests {
 
         // TODO: 利用上述更新语法(update syntax)和模板 `order_template`，
         // 创建你自己的订单(order)结构体实例!并使其通过下面的测试。
-        // let your_order =
+        let mut your_order = create_order_template();
+        your_order.name = String::from("Hacker in Rust");
+        your_order.count = 1;
 
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
